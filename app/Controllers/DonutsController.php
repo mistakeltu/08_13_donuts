339658406964872345784704a9977ab0e135ec55
Karolis <karolisdarbinis2@gmail.com>
2023-08-15 00:46:30 +0300

    Create form

--- app/Controllers/DonutsController.php
+++ app/Controllers/DonutsController.php
@@ -28,6 +28,7 @@ class DonutsController
     {
         return App::view('donuts/create', [
             'pageTitle' => 'Create new donut',
+            'coating' => $this->coating,
         ]);
     }
 }
